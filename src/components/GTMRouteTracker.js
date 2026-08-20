88da8e962724374e9a8792c0de8750c52a3c9427
dev-saiful <saifulcseian@gmail.com>
2026-08-20 22:38:36 +0600

    Refactor GTMRouteTracker to remove unused searchParams and simplify page_path construction

--- src/components/GTMRouteTracker.js
+++ src/components/GTMRouteTracker.js
@@ -1,24 +1,21 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export default function GTMRouteTracker() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     if (typeof window === "undefined" || !window.dataLayer) return;
 
-    const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : "");
-
     window.dataLayer.push({
       event: "page_view",
-      page_path: url,
+      page_path: pathname + window.location.search,
       page_location: window.location.href,
       page_title: document.title,
     });
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
